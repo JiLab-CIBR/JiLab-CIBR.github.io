862736e48b3abc4edea5ab362e9e74134e3e47f9
LSQamI <132429744+LSQamI@users.noreply.github.com>
2025-06-17 13:01:05 +0000

    Deploying to gh-pages from @ JiLab-CIBR/JiLab-CIBR.github.io@c9d1c2985d928224169af88adcfa5676ae150622 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-plotly-js",
         
