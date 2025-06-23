55c6c745bfa9ec6699456702493678832d6be95f
AEMC2 <47979312+AEMC2@users.noreply.github.com>
2025-06-23 06:38:15 +0000

    Deploying to gh-pages from @ JiLab-CIBR/JiLab-CIBR.github.io@90cd68699b3515221e6d6865cfd01af56187deab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,8 +423,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-an-emergent-low-rank-neural-architecture-for-manual-interception-of-moving-targets",
+          title: 'An emergent low-rank neural architecture for manual interception of moving targets',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
